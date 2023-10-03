68409123df9ce95d15976e6fd547f20a4a77b5da
Niranjan B <niranjannb7777@gmail.com>
2023-10-03 14:50:45 +0530

    feat: maps created

--- src/cutetext/factory.py
+++ src/cutetext/factory.py
@@ -1,11 +1,11 @@
 from .consts import TINY_TEXT,BLACK_BUBBLE_TEXT, BUBBLE_TEXT
-from .converters import TinyTextConverter
+from .converters import TinyTextConverter, BlackBubbleTextConverter, BubbleTextConverter
 
 class CuteTextConverter:
     def _get_converter(self, format: TINY_TEXT):
         if format == TINY_TEXT:
             return TinyTextConverter()
         if format == BUBBLE_TEXT:
-            return TinyTextConverter()
+            return BubbleTextConverter()
         if format == BLACK_BUBBLE_TEXT:
-            return TinyTextConverter()
+            return BlackBubbleTextConverter()
