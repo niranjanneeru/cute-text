9e080d192c396bed26083f9acd1c62d16ba2a897
Niranjan B <niranjannb7777@gmail.com>
2023-10-03 14:19:19 +0530

    feat: black bubble and bubble text added

--- src/cutetext/converters.py
+++ src/cutetext/converters.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from . import tiny_text_map
+from .maps import tiny_text_map, black_bubble_text, bubble_text_map
 
 class ConverterInterface(ABC):
     @abstractmethod
@@ -10,3 +10,13 @@ class TinyTextConverter(ConverterInterface):
 
     def convert(self, text: str) -> str:
         return text.translate(tiny_text_map)
+
+class BubbleTextConverter(ConverterInterface):
+
+    def convert(self, text: str) -> str:
+        return text.translate(bubble_text_map)
+
+class BlackBubbleTextConverter(ConverterInterface):
+
+    def convert(self, text: str) -> str:
+        return text.translate(black_bubble_text)
